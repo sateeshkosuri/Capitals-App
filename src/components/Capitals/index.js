14e1043743baa2115d03212a18ca58b4a64b7fe2
Sateesh <sateeshkosuri1997@gmail.com>
2022-12-13 10:34:19 +0000

    Capitals App tests

---
+++ src/components/Capitals/index.js
@@ -0,0 +1,74 @@
+import {Component} from 'react'
+import './index.css'
+
+const countryAndCapitalsList = [
+  {
+    id: 'NEW_DELHI',
+    capitalDisplayText: 'New Delhi',
+    country: 'India',
+  },
+  {
+    id: 'LONDON',
+    capitalDisplayText: 'London',
+    country: 'United Kingdom',
+  },
+  {
+    id: 'PARIS',
+    capitalDisplayText: 'Paris',
+    country: 'France',
+  },
+  {
+    id: 'KATHMANDU',
+    capitalDisplayText: 'Kathmandu',
+    country: 'Nepal',
+  },
+  {
+    id: 'HELSINKI',
+    capitalDisplayText: 'Helsinki',
+    country: 'Finland',
+  },
+]
+
+class Capitals extends Component {
+  state = {
+    activeCapitalId: countryAndCapitalsList[0].id,
+  }
+
+  onChangeSelect = event => {
+    this.setState({activeCapitalId: event.target.value})
+  }
+
+  getCountry = () => {
+    const {activeCapitalId} = this.state
+    const activeCountryAndCapital = countryAndCapitalsList.find(
+      eachCapital => eachCapital.id === activeCapitalId,
+    )
+
+    return activeCountryAndCapital.country
+  }
+
+  render() {
+    const {activeCapitalId} = this.state
+    const country = this.getCountry(activeCapitalId)
+
+    return (
+      <div className="capital-app-container">
+        <div className="capital-card">
+          <h1 className="main-heading">Countries And Capitals</h1>
+          <div className="select-container">
+            <select className="select" onChange={this.onChangeSelect}>
+              {countryAndCapitalsList.map(eachItem => (
+                <option key={eachItem.id} value={eachItem.id}>
+                  {eachItem.capitalDisplayText}
+                </option>
+              ))}
+            </select>
+            <p className="question">is capital of which country?</p>
+          </div>
+          <p className="country-name">{country}</p>
+        </div>
+      </div>
+    )
+  }
+}
+export default Capitals
